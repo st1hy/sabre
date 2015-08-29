e3163b058362a92c47cbe8f8013994131c811b36
st1hy <st1hy@outlook.com>
2015-08-29 22:18:58 +0200

    Moved gesture detector to independent repository replacing it with dependency.

--- gesturedetector/src/main/java/com/github/st1hy/gesturedetector/Config.java
+++
@@ -1,8 +0,0 @@
-package com.github.st1hy.gesturedetector;
-
-public final class Config {
-    /**
-     * Changes logging options of gesture detector.
-     */
-    public static boolean DEBUG = BuildConfig.DEBUG;
-}
--- gesturedetector/src/main/java/com/github/st1hy/gesturedetector/MultipleGestureListener.java
+++
@@ -1,5 +0,0 @@
-package com.github.st1hy.gesturedetector;
-
-public interface MultipleGestureListener extends ClickDetector.Listener, TranslationDetector.Listener, ScaleDetector.Listener, RotationDetector.Listener, FlingDetector.Listener, MatrixTransformationDetector.Listener {
-
-}
